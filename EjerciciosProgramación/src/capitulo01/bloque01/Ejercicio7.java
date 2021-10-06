89b8898d6ba8e9f634a67b6a2abd9f0d9eb959a0
JoseRst <josemanuel97sanchez@gmail.com>
2021-10-06 17:15:38 +0200

    ejercicio7

--- EjerciciosProgramación/src/capitulo01/bloque01/Ejercicio7.java
+++ EjerciciosProgramación/src/capitulo01/bloque01/Ejercicio7.java
@@ -1,10 +1,21 @@
 package capitulo01.bloque01;
 
+import javax.swing.JOptionPane;
+
 public class Ejercicio7 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String str = JOptionPane.showInputDialog("Introduce un número: ");
+		int var = Integer.parseInt(str);
+		
+		if ((var & 1) == 1) {
+			System.out.println("El  numero " + var + " es impar.");
+		}
+		else {
+			System.out.println("El  numero " + var + " es par.");
+		}
 
-	}
+}
 
 }
