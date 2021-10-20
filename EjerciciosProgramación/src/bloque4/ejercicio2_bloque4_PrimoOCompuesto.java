374362c893095d57f7c0f3f4cfea570f3325cc5a
JoseRst <josemanuel97sanchez@gmail.com>
2021-10-20 11:01:33 +0200

    actualizado

--- EjerciciosProgramación/src/bloque4/ejercicio2_bloque4_PrimoOCompuesto.java
+++ EjerciciosProgramación/src/bloque4/ejercicio2_bloque4_PrimoOCompuesto.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class ejercicio2_bloque4_PrimoOCompuesto {
 
 	public static void main(String[] args) {
-		int limitInf = 0, limitSup = 0;
-
+		int limitInf = 0, limitSup;
+		
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("Limite inferior");
@@ -14,25 +14,26 @@ public class ejercicio2_bloque4_PrimoOCompuesto {
 
 		System.out.println("Limite superior");
 		limitSup = sc.nextInt();
-
-		while (limitInf < limitSup) {
-
-			boolean numeroEsPrimo = true;
+		int num = limitInf+1;
+		boolean numeroEsPrimo = true;
+		
+		do {	
 			int i = 2;
-			while (i < limitInf) {
-				if (limitInf % i == 0) {
+			while (i <(num/2+1)) {
+				if (num % i == 0) {
 					numeroEsPrimo = false;
 					break;
 				}
 				i++;
 			}
-
+		
 			if (numeroEsPrimo == true) {
-				System.out.println("El número " + limitInf + " es primo.");
+				System.out.println("El número " + num + " es primo.");
 			}
-			limitInf++;
+				else {
+				System.out.println("El numero " + num + " es compuesto");
+			}
 
-		}
-
+		} while (num<limitSup);
 	}
 }
--- EjerciciosProgramación/src/bloque4/ejercicio3_bloque4_Tablas.java
+++ EjerciciosProgramación/src/bloque4/ejercicio3_bloque4_Tablas.java
@@ -5,20 +5,19 @@ public class ejercicio3_bloque4_Tablas {
 	public static void main(String[] args) {
 		int i = 1;
 		int factor = 1;
-		
+
 		while (factor < 16) {
-		
-		while (i < 11) {
-		
-			System.out.println(factor + " x " + i + " = " + (i * factor));
-			 i++ ;
-			 if (i==11){
-				 System.out.println("");
-			 }
-		}
-			factor++; 
+
+			while (i < 11) {
+
+				System.out.println(factor + " x " + i + " = " + (i * factor));
+				i++;
+				if (i == 11) {
+					System.out.println("");
+				}
+			}
+			factor++;
 			i = 1;
 		}
 	}
 }
-
